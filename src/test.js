4a6c9e2ee0657151a3ee042f678e5af2ce12cd8c
Omri Uday <93121703+OmriUday@users.noreply.github.com>
2022-04-17 14:06:39 +0300

    Add files via upload

---
+++ src/test.js
@@ -0,0 +1,10 @@
+const myObj = {
+  firstName: 'Omri',
+  lastName: 'Uday'
+}
+
+const myFirstNameVar = myObj.firstName;
+const myLastNameVar = myObj.lastName;
+
+console.log(myFirstNameVar);
+console.log(myLastNameVar);
